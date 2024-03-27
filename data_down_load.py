83610ea8218327ab60db44b6007c66c24031fd12
Happy Buzaaba <hbuzaaba@gmail.com>
2024-03-27 11:22:43 -0400

    added a file to load data

--- data_down_load.py
+++ data_down_load.py
@@ -7,4 +7,7 @@ os.makedirs(save_dir, exist_ok=True)
 
 save_path = os.path.join(os.getcwd(), save_dir)
 
-dataset = load_dataset("castorini/wura", output_path=save_path, verification_mode="no_checks")
+dataset = load_dataset("castorini/wura", verification_mode="no_checks")
+
+for split in dataset.keys():
+    dataset[split].save_to_disk(os.path.join(save_path, split))
