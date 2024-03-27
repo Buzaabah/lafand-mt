d1a10d79a2b974eb36158ee462043ec011903e65
Happy Buzaaba <hbuzaaba@gmail.com>
2024-03-27 11:03:17 -0400

    added a file to load data

--- data_down_load.py
+++ data_down_load.py
@@ -1,3 +1,10 @@
+import os
+
 from datasets import load_dataset
 
-dataset = load_dataset("castorini/wura", verification_mode="no_checks")
+save_dir = "datasets"
+os.makedirs(save_dir, exist_ok=True)
+
+save_path = os.path.join(os.getcwd(), save_dir)
+
+dataset = load_dataset("castorini/wura", cache_dir=save_path, verification_mode="no_checks")
