a3ea23dc6b727d3de90a24ccf6886121eff154f7
Happy Buzaaba <hbuzaaba@gmail.com>
2024-03-27 11:36:25 -0400

    added a file to load data

--- data_down_load.py
+++ data_down_load.py
@@ -1,13 +1,15 @@
-import os
+#import os
 
 from datasets import load_dataset
 
-save_dir = "datasets"
-os.makedirs(save_dir, exist_ok=True)
+dataset_dir = "datasets"
+#os.makedirs(save_dir, exist_ok=True)
 
-save_path = os.path.join(os.getcwd(), save_dir)
+#save_path = os.path.join(os.getcwd(), save_dir)
 
-dataset = load_dataset("castorini/wura", verification_mode="no_checks")
+dataset = load_dataset("castorini/wura", data_dir=dataset_dir)
 
-for split in dataset.keys():
-    dataset[split].save_to_disk(os.path.join(save_path, split))
+#for split in dataset.keys():
+#    dataset[split].save_to_disk(os.path.join(save_path, split))
+
+print(f"WURA dataset downloaded and saved to: {dataset_dir}")
