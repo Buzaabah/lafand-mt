0721efa83277afa23959f000b41937819c58aa7d
Happy Buzaaba <hbuzaaba@gmail.com>
2024-04-11 09:29:43 -0400

    changed model download

--- model_download.py
+++ model_download.py
@@ -1,4 +1,8 @@
 from transformers import T5ForConditionalGeneration, AutoTokenizer
 
-model = T5ForConditionalGeneration.from_pretrained('google/byt5-xxl')
-tokenizer = AutoTokenizer.from_pretrained('google/byt5-xxl')
+#model = T5ForConditionalGeneration.from_pretrained('google/byt5-xxl')
+#tokenizer = AutoTokenizer.from_pretrained('google/byt5-xxl')
+
+
+model = T5ForConditionalGeneration.from_pretrained('google/byt5-base')
+tokenizer = AutoTokenizer.from_pretrained('google/byt5-base')
